b08f5b6d558bef49bbba3611461cf5a7a0503668
Leonardo <60203599+leonardopollesel@users.noreply.github.com>
2020-05-16 22:08:16 +0200

    up with black

--- SK_Learn.py
+++ SK_Learn.py
@@ -10,13 +10,18 @@ def boston_function():
     reg = LinearRegression()  # create regression object
     reg.fit(dataset.data, dataset.target)  # fit dataset
     result = pd.DataFrame(
-    abs(reg.coef_), dataset.feature_names, columns=["coef"]
+        abs(reg.coef_), dataset.feature_names, columns=["coef"]
     )  # take regression coefficients
     result.sort_values(
-    by=["coef"], ascending=False, inplace=True
+        by=["coef"], ascending=False, inplace=True
     )  # sort regression coefficients
-    return print("The most affecting fator is", result.index[0], "with a value of :", result.coef[0])  # print best coefficient
+    return print(
+        "The most affecting fator is",
+        result.index[0],
+        "with a value of :",
+        result.coef[0],
+    )  # print best coefficient
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     boston_function()
